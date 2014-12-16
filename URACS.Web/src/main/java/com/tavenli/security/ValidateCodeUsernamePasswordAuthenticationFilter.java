b54e5d832bcd27b420dfca19a4c132a56a6db777
Tenchael <tzz1002@gmail.com>
2014-12-16 16:49:37 +0800

    first commit

--- URACS.Web/src/main/java/com/tavenli/security/ValidateCodeUsernamePasswordAuthenticationFilter.java
+++ URACS.Web/src/main/java/com/tavenli/security/ValidateCodeUsernamePasswordAuthenticationFilter.java
@@ -3,7 +3,7 @@ package com.tavenli.security;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationServiceException;
--- URACS.Web/src/main/java/com/tavenli/utils/ValidatorUtil.java
+++ URACS.Web/src/main/java/com/tavenli/utils/ValidatorUtil.java
@@ -6,7 +6,7 @@ import java.util.regex.PatternSyntaxException;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class ValidatorUtil {
 
