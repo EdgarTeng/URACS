fdeca6ce9bd6e4e8fdf7331879fcf3b142d29d1d
Tenchael <tzz1002@gmail.com>
2015-01-16 20:12:17 +0800

    first commit

--- URACS.Web/src/main/java/com/tavenli/interceptors/UAuthorizeInterceptor.java
+++ URACS.Web/src/main/java/com/tavenli/interceptors/UAuthorizeInterceptor.java
@@ -16,32 +16,35 @@ import org.springframework.web.servlet.ModelAndView;
  */
 public class UAuthorizeInterceptor implements HandlerInterceptor {
 
-	private static Logger logger = LoggerFactory.getLogger(UAuthorizeInterceptor.class);
-	
+	private static Logger logger = LoggerFactory
+			.getLogger(UAuthorizeInterceptor.class);
+
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		
-		//你可以在这里做一些拦截处理，当然你也可以什么都不做，如果您只关注 Spring Security，可以不关注 HandlerInterceptor 接口
-		
-		//String path = request.getContextPath();
-		//response.sendRedirect(path + "/index");
-				
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+
+		// 你可以在这里做一些拦截处理，当然你也可以什么都不做，如果您只关注 Spring Security，可以不关注
+		// HandlerInterceptor 接口
+
+		// String path = request.getContextPath();
+		// response.sendRedirect(path + "/index");
+
 		return true;
-		
+
 	}
 
 	@Override
-	public void postHandle(HttpServletRequest request,HttpServletResponse response, Object handler,ModelAndView modelAndView) throws Exception {
-		
-		
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
 	}
 
 	@Override
-	public void afterCompletion(HttpServletRequest request,	HttpServletResponse response, Object handler, Exception ex) throws Exception {
-		
-		
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+
 	}
 
-
-	
 }
